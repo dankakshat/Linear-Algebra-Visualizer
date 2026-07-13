19f6091bb1b6970eaa34a0adadad25c32dea7332
Akshat Awasthi <akshatorignal@gmail.com>
2026-07-13 23:57:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/column-picture/',
+  base: '/Linear-Algebra-Visualizer/',
   plugins: [react(), tailwindcss()],
 })
